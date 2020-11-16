5f5a7c815d47394099e06c76789e2314790304e9
Marcelo <augustomnzteixeira@gmail.com>
2020-11-16 13:47:23 -0300

    fazendo o carregamento das imagens a partir do código base64

--- js/alimentar-modal.js
+++ js/alimentar-modal.js
@@ -1,33 +1,40 @@
 $(document).on('click', '#button-alimentar-modal-arquivo-imagem', function (e) {
-
-    let imagem3 = document.createElement('img');
-    imagem3.setAttribute('src', '../img/imagem3.jpg');
-    imagem3.setAttribute('width', 240);
-    imagem3.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
-    $('#div-imagens').append(imagem3);
-
-    let imagem4 = document.createElement('img');
-    imagem4.setAttribute('src', '../img/imagem4.jpg');
-    imagem4.setAttribute('width', 240);
-    imagem4.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
-    $('#div-imagens').append(imagem4);
-
-    let imagem5 = document.createElement('img');
-    imagem5.setAttribute('src', '../img/imagem5.jpg');
-    imagem5.setAttribute('width', 240);
-    imagem5.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
-    $('#div-imagens').append(imagem5);
-
-    let imagem6 = document.createElement('img');
-    imagem6.setAttribute('src', '../img/imagem6.jpg');
-    imagem6.setAttribute('width', 240);
-    imagem6.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
-    $('#div-imagens').append(imagem6);
+    carregarImagemArquivo('../img/imagem3.jpg');
+    carregarImagemArquivo('../img/imagem4.jpg');
+    carregarImagemArquivo('../img/imagem5.jpg');
+    carregarImagemArquivo('../img/imagem6.jpg');
 
     $('#modal-de-imagens').modal('show');
 });
 
+function carregarImagemArquivo (caminho) {
+    let imagem = document.createElement('img');
+    imagem.setAttribute('src', caminho);
+    imagem.setAttribute('width', 240);
+    imagem.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
+    $('#div-imagens').append(imagem);
+}
+
+$(document).on('click', '#button-alimentar-modal-codigo-base64', function (e) {
+    carregarImagemBase64('../arquivos/imagem3.txt');
+    carregarImagemBase64('../arquivos/imagem4.txt');
+    carregarImagemBase64('../arquivos/imagem5.txt');
+    carregarImagemBase64('../arquivos/imagem6.txt');
+
+    $('#modal-de-imagens').modal('show');
+});
+
+function carregarImagemBase64 (caminho) {
+    $.get(caminho, 'text', function (conteudo) {
+        let imagem = document.createElement('img');
+        imagem.setAttribute('src', conteudo);
+        imagem.setAttribute('width', 240);
+        imagem.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
+        $('#div-imagens').append(imagem);
+    });
+}
+
 $(document).on('click', '#fechar-modal-de-imagens', function (e) {
     $('.imagem-modal').remove();
-})
+});
 
