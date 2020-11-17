a57a59a6fc698be25420e35b3ffb96f6bf39884a
Marcelo <augustomnzteixeira@gmail.com>
2020-11-17 16:59:49 -0300

    imagens com checkbox

--- js/alimentar-modal.js
+++ js/alimentar-modal.js
@@ -5,17 +5,12 @@ $(document).on('click', '#button-alimentar-modal-arquivo-imagem', function (e) {
     carregarImagemArquivo('../img/imagem4.jpg');
     carregarImagemArquivo('../img/imagem5.jpg');
     carregarImagemArquivo('../img/imagem6.jpg');
+    carregarImagemArquivo('../img/imagem7.jpg');
+    carregarImagemArquivo('../img/imagem8.jpg');
+    carregarImagemArquivo('../img/imagem9.jpg');
 
     $('#modal-de-imagens').modal('show');
 });
-
-function carregarImagemArquivo (caminho) {
-    let imagem = document.createElement('img');
-    imagem.setAttribute('src', caminho);
-    imagem.setAttribute('width', 240);
-    imagem.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
-    $('#div-imagens').append(imagem);
-}
 
 $(document).on('click', '#button-alimentar-modal-codigo-base64', function (e) {
     carregarImagemBase64('../arquivos/imagem1.txt');
@@ -24,9 +19,55 @@ $(document).on('click', '#button-alimentar-modal-codigo-base64', function (e) {
     carregarImagemBase64('../arquivos/imagem4.txt');
     carregarImagemBase64('../arquivos/imagem5.txt');
     carregarImagemBase64('../arquivos/imagem6.txt');
+    carregarImagemBase64('../arquivos/imagem7.txt');
+    carregarImagemBase64('../arquivos/imagem8.txt');
+    carregarImagemBase64('../arquivos/imagem9.txt');
 
     $('#modal-de-imagens').modal('show');
 });
+
+$(document).on('click', '#button-alimentar-modal-imagem-checkbox', function (e) {
+    carregarImagemCheckBox('../arquivos/imagem1.txt', 'Fullmetal Alchemist');
+    carregarImagemCheckBox('../arquivos/imagem2.txt', 'Death Note');
+    carregarImagemCheckBox('../arquivos/imagem3.txt', 'Dragon Ball');
+    carregarImagemCheckBox('../arquivos/imagem4.txt', 'Naruto');
+    carregarImagemCheckBox('../arquivos/imagem5.txt', 'Yu Yu Hakusho');
+    carregarImagemCheckBox('../arquivos/imagem6.txt', 'InuYasha');
+    carregarImagemCheckBox('../arquivos/imagem7.txt', 'Cavaleiros do Zodíaco');
+    carregarImagemCheckBox('../arquivos/imagem8.txt', 'Shingeki no Kyojin');
+    carregarImagemCheckBox('../arquivos/imagem9.txt', 'Digimon');
+
+    $('#modal-de-imagens').modal('show');
+});
+
+function carregarImagemCheckBox (caminho, value) {
+    $.get(caminho, 'text', function (conteudo) {
+        let label = document.createElement('label');
+        label.setAttribute('class', 'image-checkbox');
+
+        let imagem = document.createElement('img');
+        imagem.setAttribute('src', conteudo);
+        imagem.setAttribute('width', 240);
+        imagem.setAttribute('class', 'img-responsive img-thumbnail preview-img imagem-modal');
+
+        let input = document.createElement('input');
+        input.setAttribute('type', 'checkbox');
+        input.setAttribute('name', 'input-imagem');
+        input.setAttribute('value', value);
+
+        label.appendChild(imagem);
+        label.appendChild(input);
+        $('#div-imagens').append(label);
+    });
+}
+
+function carregarImagemArquivo (caminho) {
+    let imagem = document.createElement('img');
+    imagem.setAttribute('src', caminho);
+    imagem.setAttribute('width', 240);
+    imagem.setAttribute('class', 'img-thumbnail preview-img imagem-modal');
+    $('#div-imagens').append(imagem);
+}
 
 function carregarImagemBase64 (caminho) {
     $.get(caminho, 'text', function (conteudo) {
---
+++ js/style-image-checkbox.js
@@ -0,0 +1,21 @@
+
+$(".image-checkbox").each(function () {
+    if ($(this).find('input[type="checkbox"]').first().attr("checked")) {
+      $(this).addClass('image-checkbox-checked');
+    }
+    else {
+      $(this).removeClass('image-checkbox-checked');
+    }
+  });
+  
+// sync the state to the input
+
+$(document).on('click', '.image-checkbox', function (e) {
+    $(this).toggleClass('image-checkbox-checked');
+    let input = $(this).find('input[type="checkbox"]');
+    input.prop("checked",!input.prop("checked"))
+    
+    console.log(input.val());
+
+    e.preventDefault();
+});
